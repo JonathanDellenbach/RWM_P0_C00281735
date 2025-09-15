d3ea443e946033aa0b4e67ec08d5d441f4303c22
JonathanDellenbach <C00281735@itcarlow.ie>
2025-09-15 12:09:49 +0100

    Xor21 with a Fault

--- src/lib/filters/xor21.test.ts
+++ src/lib/filters/xor21.test.ts
@@ -3,7 +3,7 @@ import { xor } from './xor21';
 
 describe('Xor filter', () => {
   it('Xors a number', () => {
-    const input = 4;
+    const input = 5;
     const expected = 17;
     expect(xor(input)).toEqual(expected);
   });
