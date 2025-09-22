16a2455ef4bb0562df61f2929d7ebda682b9ec9c
JonathanDellenbach <C00281735@itcarlow.ie>
2025-09-22 11:10:34 +0100

    Results artefat sign-off

--- src/routes/combined/+page.svelte
+++ src/routes/combined/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { combinedFilter } from '$lib/filters/combined/combined';
-  let raw = '1 3 5 7 9';
-  $: input = raw.trim().split(/\s+/).filter(Boolean).map(Number);
-  $: output = combinedFilter(input);
+  import { multipleXors } from '$lib/filters/xor21';
+
+  let c00286807ORaw = "5 10 120 58 2017 231";
+  $: c00286807OInput = c00286807ORaw.trim().split(/\s+/).filter(Boolean).map(Number);
+  $: c00286807Output = combinedFilter(c00286807OInput);
+
+  let c00281735Raw = "4 2 8 3 9 4 10";
+  $: c00281735Input = c00281735Raw.trim().split(/\s+/).filter(Boolean).map(Number);
+  $: c00281735Output = multipleXors(c00281735Input);
 </script>
 
+<h1>Combined (mine -- peer)</h1>
+<label>Edward Input: <input bind:value={c00286807ORaw} /></label>
+<p>{"Edward's Test BiggestWithSameDigits: " + c00286807Output.join(" ")}</p>
+<label>Jonathan Input: <input bind:value={c00281735Raw} /></label>
+<p>{"Jonathan's Test XOR21: " + c00281735Output.join(" ")}</p>
 <a href="/">Go to sequence filter demo</a>
-
-<h1>Combined (mine → peer)</h1>
-<label>Input <input bind:value={raw} /></label>
-<p>Output: {output.join(' ')}</p>
